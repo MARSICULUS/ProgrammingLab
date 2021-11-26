1ef52372194884efbb83763897b3473009dfe3bd
MARSICULUS <91570512+MARSICULUS@users.noreply.github.com>
2021-11-26 15:05:06 +0000

    esercizio 5 punto a

---
+++ Lezioni/lezione05/esercizio05 .py
@@ -0,0 +1,41 @@
+class CSVFile:
+
+    #inizializzo classe con nome del file
+    def __init__(self, nome_file):
+        #attributo name
+        try:
+            self.name = nome_file
+            file_csv = open(self.name , 'r')
+        except FileNotFoundError:
+            print('Il file indicato non è stato TROVATO')
+            print('il nome inserito era "{}"'.format(nome_file))
+            self.name = "defolt.csv"
+        except Exception as e:
+            print('errore generico: "{}"'.format(e))
+            self.name = "defolt.csv"
+
+    
+    #metodo restituisce una lista di liste conteneti i dati(sono stringhe)
+    def get_data(self):
+        #lista che conterrà i dati
+        all_data = []
+        #apro file
+        file_csv = open(self.name , 'r')
+
+        #guardo il file linea per linea
+        for line in file_csv:
+            #la salvo in all_data, con le modifiche del caso
+            line = line.strip('\n')
+            elemento = line.split(',')
+            if(elemento[0] != 'Date'):
+                all_data.append(elemento)
+        
+        #chiudo il file e ritorno all_data
+        file_csv.close()
+        return all_data
+        
+
+myfile = CSVFile('nome_non_esistente.csv')
+#print(myfile)
+#print(myfile.name)
+print(*myfile.get_data(), sep = '\n')
